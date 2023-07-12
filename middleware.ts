fc2019fe0b0b6826721b3d2036de284632f8bca8
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2023-07-12 09:44:01 +0200

    Set api token security to classify pathes only.

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export const config = {
-  matcher: ["/api/", "/api/classify/bytag/:path*", "/api/classify/bytext"],
+  matcher: ["/api/classify/:path*"],
 };
 
 export function middleware(req: NextRequest) {
